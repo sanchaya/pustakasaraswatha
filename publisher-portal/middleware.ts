cd9bb44d3657e088dd2e192efe4f7dac8ffc253a
AAADHIRAAA <padmapriyaindia2004@gmail.com>
2024-04-19 09:37:25 +0530

    ignoredRoutes updated

--- publisher-portal/middleware.ts
+++ publisher-portal/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes:['/','/About','/Contact'],
+    ignoredRoutes: ["/facebook.png", "/twitter.webp", "/sanchaya.jpg"]
 });
 
 export const config = {
